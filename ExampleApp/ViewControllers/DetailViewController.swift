7434d9ff5fbf38972d9cb96c192407042a618973
Jiri Zoudun <jiri.zoudun@etnetera.cz>
2020-02-28 14:33:56 +0100

    Added example project for modal presentation. (bug/modals)

---
+++ ExampleApp/ViewControllers/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  ExampleApp
+//
+//  Created by Jiří Zoudun on 21/02/2020.
+//  Copyright © 2020 Etnetera a. s. All rights reserved.
+//
+
+import Foundation
---
+++ ExampleApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Example
+//
+//  Created by Jiří Zoudun on 21/02/2020.
+//  Copyright © 2020 Etnetera a. s. All rights reserved.
+//
+
+import Foundation
